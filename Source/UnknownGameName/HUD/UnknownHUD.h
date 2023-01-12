cb261b491d1159435a06ff56911c0a40a99cf521
Cohnny Flobrandt <tjonk11@gmail.com>
2023-01-12 19:56:03 +0100

    Added shells and shellsounds

---
+++ Source/UnknownGameName/HUD/UnknownHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "UnknownHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNKNOWNGAMENAME_API AUnknownHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/UnknownGameName/PlayerController/UnknownPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "UnknownPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNKNOWNGAMENAME_API AUnknownPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
