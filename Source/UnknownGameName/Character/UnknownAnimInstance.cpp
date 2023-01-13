ae9ce1d0c67ea95a58ef8d59df2b446aee794ddd
Cohnny Flobrandt <tjonk11@gmail.com>
2023-01-13 11:21:29 +0100

    ..

--- Source/UnknownGameName/Character/UnknownAnimInstance.cpp
+++ Source/UnknownGameName/Character/UnknownAnimInstance.cpp
@@ -67,22 +67,5 @@ void UUnknownAnimInstance::NativeUpdateAnimation(float DeltaTime)
 
 		FTransform RightHandTransform = EquippedWeapon->GetWeaponMesh()->GetSocketTransform(FName("hand_r"), ERelativeTransformSpace::RTS_World);
 		RightHandRotation = UKismetMathLibrary::FindLookAtRotation(RightHandTransform.GetLocation(), RightHandTransform.GetLocation() + (RightHandTransform.GetLocation() - UnknownCharacter->GetHitTarget()));
-
-		FTransform MuzzleTipTransform = EquippedWeapon->GetWeaponMesh()->GetSocketTransform(FName("MuzzleFlash"), ERelativeTransformSpace::RTS_World);
-		FVector MuzzleX(FRotationMatrix(MuzzleTipTransform.GetRotation().Rotator()).GetUnitAxis(EAxis::X));
-
-		DrawDebugLine(
-			GetWorld(),
-			MuzzleTipTransform.GetLocation(),
-			MuzzleTipTransform.GetLocation() + MuzzleX * 1000.f,
-			FColor::Red
-		);
-
-		DrawDebugLine(
-			GetWorld(),
-			MuzzleTipTransform.GetLocation(),
-			UnknownCharacter->GetHitTarget(),
-			FColor::Orange
-		);
 	}
 }
