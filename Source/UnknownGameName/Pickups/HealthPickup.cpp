0f2cceea1f52ebbf37b01652570b1bfe8896b21f
Cohnny Flobrandt <tjonk11@gmail.com>
2023-02-07 17:14:57 +0100

    Added health pickup

---
+++ Source/UnknownGameName/Pickups/HealthPickup.cpp
@@ -0,0 +1,39 @@
+#include "HealthPickup.h"
+#include "UnknownGameName/Character/UnknownCharacter.h"
+#include "UnknownGameName/UnknownComponents/BuffComponent.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraComponent.h"
+
+AHealthPickup::AHealthPickup()
+{
+	bReplicates = true;
+
+	PickupEffectComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("PickupEffectComponent"));
+	PickupEffectComponent->SetupAttachment(RootComponent);
+}
+
+void AHealthPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	AUnknownCharacter* UnknownCharacter = Cast<AUnknownCharacter>(OtherActor);
+	if (UnknownCharacter)
+	{
+		
+	}
+	Destroy();
+}
+
+void AHealthPickup::Destroyed()
+{
+	if (PickupEffect)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(
+			this,
+			PickupEffect,
+			GetActorLocation(),
+			GetActorRotation()		
+		);
+	}
+	Super::Destroyed();
+}
