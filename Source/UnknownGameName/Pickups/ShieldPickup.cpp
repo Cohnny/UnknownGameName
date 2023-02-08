0a9bd5becf3faafcaa76bad88543009b8b3a5cb1
Cohnny Flobrandt <tjonk11@gmail.com>
2023-02-08 03:58:35 +0100

    Added more pickups

---
+++ Source/UnknownGameName/Pickups/ShieldPickup.cpp
@@ -0,0 +1,19 @@
+#include "ShieldPickup.h"
+#include "UnknownGameName/Character/UnknownCharacter.h"
+#include "UnknownGameName/UnknownComponents/BuffComponent.h"
+
+void AShieldPickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+	AUnknownCharacter* UnknownCharacter = Cast<AUnknownCharacter>(OtherActor);
+	if (UnknownCharacter)
+	{
+		UBuffComponent* Buff = UnknownCharacter->GetBuff();
+		if (Buff)
+		{
+			Buff->ReplenishShield(ShieldReplenishAmount, ShieldReplenishTime);
+		}
+	}
+	Destroy();
+}
